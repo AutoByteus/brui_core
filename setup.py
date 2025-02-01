e770359d1f3cfe096fdaa913e43b37567e050806
Ryan Zheng <nogrethumphrey@gmail.com>
2025-02-01 06:21:21 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brui_core',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=[
         'playwright==1.42.0',
